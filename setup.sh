62354dcf571df4c3563a877d332d10b4cefc94b8
jaybanuan <40902617+jaybanuan@users.noreply.github.com>
2022-04-15 23:47:21 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,28 @@
 #!/bin/bash -x
 
+apt-get update
+
 PKG=""
 
 # Development tools
-PKG="$PKG git make curl"
+PKG="$PKG git make curl meld"
 
-sudo apt-get install -y $PKG
+# install packages
+apt-get install -y $PKG
 
+# install Google Chrome
 curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+# install Visual Studio Code
+
+# install docker
+apt-get install -y ca-certificates curl gnupg lsb-release
+
+
+# install kubectl
+snap install kubectl --classic
+
+# install MiniKube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+dpkg -i minikube_latest_amd64.deb
