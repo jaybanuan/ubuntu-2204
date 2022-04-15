e7a58cccf854c69a03ee921ed880d7fad19086da
jaybanuan <40902617+jaybanuan@users.noreply.github.com>
2022-04-16 00:51:07 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -x
 
+# update packages
 apt-get update
+apt-get dist-upgrade
 
+# init variables
 PKG=""
 
 # Development tools
-PKG="$PKG git make curl meld"
+PKG="$PKG git make curl wget meld"
 
 # install packages
 apt-get install -y $PKG
@@ -15,14 +18,28 @@ curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.de
 apt-get install -y ./google-chrome-stable_current_amd64.deb
 
 # install Visual Studio Code
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
 
 # install docker
 apt-get install -y ca-certificates curl gnupg lsb-release
-
-
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+ 
 # install kubectl
 snap install kubectl --classic
 
 # install MiniKube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 dpkg -i minikube_latest_amd64.deb
+
+# install VLC
+snap install vlc
