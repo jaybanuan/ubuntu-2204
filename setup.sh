bea859cd38d4d5e884a428a01d849c248a5aa84c
jaybanuan <40902617+jaybanuan@users.noreply.github.com>
2022-04-15 23:33:56 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,8 @@
 PKG=""
 
 # Development tools
-PKG="$PKG git make "
+PKG="$PKG git make curl"
+
+
+curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
