8c089c532183833b7713ef405fb0a54ef2c2d317
Masashi Takahashi <18286485+mtakahashiif@users.noreply.github.com>
2022-04-16 09:20:35 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,14 +18,14 @@ curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.de
 apt-get install -y ./google-chrome-stable_current_amd64.deb
 
 # install Visual Studio Code
-sudo apt-get install wget gpg
+sudo apt-get install -y wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https
+sudo apt install -y apt-transport-https
 sudo apt update
-sudo apt install code # or code-insiders
+sudo apt install -y code # or code-insiders
 
 # install docker
 apt-get install -y ca-certificates curl gnupg lsb-release
