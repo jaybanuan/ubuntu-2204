e4638150b9cca99ddecba1f1c11a5c6ef347994b
jaybanuan <40902617+jaybanuan@users.noreply.github.com>
2022-04-27 07:28:06 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ sudo apt-get dist-upgrade -y
 ##############################################################################
 # install general packages
 
-sudo apt-get install -y git make curl wget meld gdebi mozc-utils-gui
+sudo apt-get install -y git make curl wget tree meld gdebi mozc-utils-gui
 
 
 ##############################################################################
